3d15dccf6d14f085dfe468576816d45d7a39c1e2
Student <nagaphani9030@gmail.com>
2025-10-07 12:39:20 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ import Footer from '../components/Footer.jsx';
 const Home = () => {
     return (
         <div className="min-h-screen">
-            <Header />
+            <Header  />
             <main>
                 <Hero />
                 <FeaturedCollections />
